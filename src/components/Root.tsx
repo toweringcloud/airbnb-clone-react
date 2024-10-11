eb368093387fe1c63e93a4d4728ea1381cfd763d
toweringcloud <toweringcloud@gmail.com>
2024-10-12 01:18:22 +0900

    signin & signup modal ui +

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,9 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
+import Header from "./Header";
+
 export default function Root() {
   return (
-    <Box bg="gray.100">
+    <Box>
+      <Header />
       <Outlet />
     </Box>
   );
