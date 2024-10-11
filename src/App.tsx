cd0d8966e984a9ed3b65492dc630f96a1b1bb1da
toweringcloud <toweringcloud@gmail.com>
2024-10-12 01:50:16 +0900

    light <-> dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 
 import router from "./router";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
 export default function App() {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 			<QueryClientProvider client={queryClient}>
 				<RouterProvider router={router} />
 			</QueryClientProvider>
